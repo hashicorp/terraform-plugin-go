0a8f31f416cde904f0ef3d995f3bef419012d75a
Paddy Carver <paddy@hashicorp.com>
2020-08-24 19:06:31 -0700

    Flesh out tftypes package a bit more.
    Our type system is starting to take shape. It leans a little more
    towards capturing the guts of cty's implementation details and smoothing
    over them with semantics using the `RawValue.Unmarshal` method.
    
    There's a bunch of ambiguity in cty types and how we get type
    information. It's not captured in cty when encoding with msgpack whether
    something is a naked set, tuple, or list, or whether it is type
    information then a value. Similarly, JSON doesn't capture whether
    something is a map, object, or type information and value. The decoder
    needs to know this information or push that problem on to the consumer.
    
    Our implementation is choosing to capture the raw ambiguity in the
    RawValue type, and then paper it over in Unmarshal based on the type
    that the user provides to us.

--- tfprotov5/tftypes/types.go
+++ tfprotov5/tftypes/types.go
@@ -2,15 +2,28 @@ package tftypes
 
 import (
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 )
 
+type unknown byte
+
 const (
-	String = Type('S')
+	UnknownValue      = unknown(0)
+	DynamicPseudoType = Type('D')
+	String            = Type('S')
+	Number            = Type('N')
+	Bool              = Type('B')
+	List              = Type('L')
+	Set               = Type('S')
+	Map               = Type('M')
+	Tuple             = Type('T')
+	Object            = Type('O')
 )
 
 type Unmarshaler interface {
-	UnmarshalTerraform5Type(t Type, v interface{}) error
+	UnmarshalTerraform5Type(RawValue) error
 }
 
 type ErrUnhandledType Type
@@ -31,13 +44,136 @@ func (r RawValue) Unmarshal(dst interface{}) error {
 		return unmarshaler.UnmarshalTerraform5Type(r.Type, r.Value)
 	}
 	switch r.Type {
-	// TODO: cast to primitives as a default behavior
 	case String:
 		if _, ok := dst.(*string); !ok {
 			return fmt.Errorf("Can't unmarshal %s into %T", r.Type, dst)
 		}
-		str := r.Value.(string)
-		dst = &str
+		*dst = r.Value.(string)
+	case Number:
+		switch r.Value.(type) {
+		case int64:
+			switch dst.(type) {
+			case *int64:
+				*dst = r.Value.(int64)
+			case *int32:
+				if r.Value.(int64) > math.MaxInt32 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(int64) < math.MinInt32 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = int32(r.Value.(int64))
+			case *int16:
+				if r.Value.(int64) > math.MaxInt16 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(int64) < math.MinInt16 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = int16(r.Value.(int64))
+			case *int8:
+				if r.Value.(int64) > math.MaxInt8 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(int64) < math.MinInt8 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = int8(r.Value.(int64))
+			case *int:
+				// int types are only guaranteed to be able to
+				// hold 32 bits; anything more is dependent on
+				// the system. Because providers need to work
+				// across architectures, we're going to ensure
+				// that only the minimum is used here. Anyone
+				// that needs more can use int64
+				if r.Value.(int64) > math.MaxInt32 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't always fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(int64) < math.MinInt32 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't always fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = int(r.Value.(int64))
+			default:
+				return fmt.Errorf("can't unmarshal %s into %T", r.Type, dst)
+			}
+		case uint64:
+			switch dst.(type) {
+			case *uint64:
+				*dst = r.Value.(uint64)
+			case *uint32:
+				if r.Value.(uint64) > math.MaxUint32 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(uint64) < math.MinUint32 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = uint32(r.Value.(uint64))
+			case *uint16:
+				if r.Value.(uint64) > math.MaxUint16 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(uint64) < math.MinUint16 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = uint16(r.Value.(uint64))
+			case *uint8:
+				if r.Value.(uint64) > math.MaxUint8 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(uint64) < math.MinUint8 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = uint8(r.Value.(uint64))
+			case *uint:
+				// uint types are only guaranteed to be able to
+				// hold 32 bits; anything more is dependent on
+				// the system. Because providers need to work
+				// across architectures, we're going to ensure
+				// that only the minimum is used here. Anyone
+				// that needs more can use uint64
+				if r.Value.(uint64) > math.MaxUint32 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't always fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(uint64) < math.MinUint32 {
+					return fmt.Errorf("can't unmarshal %s into %T: %d doesn't always fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = uint(r.Value.(uint64))
+			default:
+				return fmt.Errorf("can't unmarshal %s into %T", r.Type, dst)
+			}
+		case float64:
+			switch dst.(type) {
+			case *float64:
+				*dst = r.Value.(float64)
+			case *float32:
+				if r.Value.(float64) > math.MaxFloat32 {
+					return fmt.Errorf("can't unmarshal %s floato %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				if r.Value.(float64) < math.MinFloat32 {
+					return fmt.Errorf("can't unmarshal %s floato %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+				}
+				*dst = float32(r.Value.(float64))
+			default:
+				return fmt.Errorf("can't unmarshal %s into %T", r.Type, dst)
+			}
+		case big.Float:
+		}
+	case Bool:
+		if _, ok := dst.(*bool); !ok {
+			return fmt.Errorf("Can't unmarshal %s into %T", r.Type, dst)
+		}
+		b := r.Value.(bool)
+		*dst = b
+	case List:
+		// TODO: handle ambiguity of msgpack types
+	case Set:
+		// TODO: handle ambiguity of msgpack types
+	case Tuple:
+		// TODO: handle ambiguity of msgpack types
+	case Map:
+		// TODO: handle ambiguity of json types
+	case Object:
+		// TODO: handle ambiguity of json types
 	}
 	return ErrUnhandledType(r.Type)
 }
@@ -47,9 +183,24 @@ type Type rune
 
 func (t Type) String() string {
 	switch t {
-	// TODO: return string version of type constants
+	case DynamicPseudoType:
+		return "tftypes.DynamicPseudoType"
 	case String:
 		return "tftypes.String"
+	case Number:
+		return "tftypes.Number"
+	case Bool:
+		return "tftypes.Bool"
+	case List:
+		return "tftypes.List"
+	case Set:
+		return "tftypes.Set"
+	case Map:
+		return "tftypes.Map"
+	case Tuple:
+		return "tftypes.Tuple"
+	case Object:
+		return "tftypes.Object"
 	}
 	return "unknown type " + strconv.QuoteRune(rune(t))
 }
