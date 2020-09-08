eede65ad3bf715019f0a59aa74189818235182f0
Paddy Carver <paddy@hashicorp.com>
2020-09-08 15:55:05 -0700

    Fix formatting.

--- tfprotov5/internal/fromproto/state.go
+++ tfprotov5/internal/fromproto/state.go
@@ -1,8 +1,8 @@
 package fromproto
 
 import (
-	"github.com/hashicorp/terraform-plugin-go/tfprotov5/internal/tfplugin5"
 	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5/internal/tfplugin5"
 )
 
 func RawState(in tfplugin5.RawState) tfprotov5.RawState {
