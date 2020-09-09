e21703814ba85d922805c0d2f22dafbf014663a8
Paddy Carver <paddy@hashicorp.com>
2020-09-08 19:27:39 -0700

    Add some handling for complex types.
    Complex types, or values that come back with type information inline,
    require some specialised handling and parsing. This gets us most of the
    way there, with several TODOs remaining to polish it up a bit before
    it's actually ready.
    
    We're going to need extensive unit tests around this logic, because it's
    probably the most dangerous area of code we have right now in terms of
    complexity.

--- tfprotov5/tftypes/types.go
+++ tfprotov5/tftypes/types.go
@@ -1,6 +1,7 @@
 package tftypes
 
 import (
+	"encoding/json"
 	"fmt"
 	"math"
 	"math/big"
@@ -135,10 +136,10 @@ func (r RawValue) Unmarshal(dst interface{}) error {
 				*(dst.(*float64)) = r.Value.(float64)
 			case *float32:
 				if r.Value.(float64) > math.MaxFloat32 {
-					return fmt.Errorf("can't unmarshal %s floato %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+					return fmt.Errorf("can't unmarshal %s into %T: %f doesn't fit in %T", r.Type, dst, r.Value, dst)
 				}
 				if r.Value.(float64) < math.SmallestNonzeroFloat32 {
-					return fmt.Errorf("can't unmarshal %s floato %T: %d doesn't fit in %T", r.Type, dst, r.Value, dst)
+					return fmt.Errorf("can't unmarshal %s into %T: %f doesn't fit in %T", r.Type, dst, r.Value, dst)
 				}
 				*(dst.(*float32)) = float32(r.Value.(float64))
 			default:
@@ -153,17 +154,219 @@ func (r RawValue) Unmarshal(dst interface{}) error {
 		}
 		*(dst.(*bool)) = r.Value.(bool)
 	case List:
-		// TODO: handle ambiguity of msgpack types
+		// this could be an actual, honest-to-goodness list, or it
+		// could be a primitive with its cty information included. We
+		// only know based on what the caller tells us they're
+		// expecting; an []interface{} is  expecting a list, a RawValue
+		// is expecting a *new* RawValue, with the Type property
+		// derived from the cty information given to us.
+		switch dst.(type) {
+		case *[]interface{}:
+			*(dst.(*[]interface{})) = r.Value.([]interface{})
+		case *RawValue:
+			val := r.Value.([]interface{})
+			if len(val) != 2 {
+				// TODO: return error
+			}
+			var typ interface{}
+			err := json.Unmarshal([]byte(val[0].(string)), &typ)
+			if err != nil {
+				// TODO: return error
+			}
+			rv, err := rawValueFromComplexType(typ, val[1])
+			if err != nil {
+				return err
+			}
+			*(dst.(*RawValue)) = rv
+		default:
+			// TODO: return error
+		}
 	case Set:
-		// TODO: handle ambiguity of msgpack types
+		// this can only be something the user decided explicitly was a
+		// Set; we can't get this from default behavior, so there's no
+		// ambiguity here.
+		switch dst.(type) {
+		case *[]interface{}:
+			*(dst.(*[]interface{})) = r.Value.([]interface{})
+		default:
+			// TODO: return error
+		}
 	case Tuple:
-		// TODO: handle ambiguity of msgpack types
+		// this can only be something the user decided explicitly was a
+		// Tuple; we can't get this from default behavior, so there's
+		// no ambiguity here.
+		switch dst.(type) {
+		case *[]interface{}:
+			*(dst.(*[]interface{})) = r.Value.([]interface{})
+		default:
+			// TODO: return error
+		}
 	case Map:
-		// TODO: handle ambiguity of json types
+		// this could be an actual, honest-to-goodness map, or it could
+		// be a value with its cty information included. We only know
+		// based on what the caller tells us they're expecting; a
+		// map[string]interface{} is expecting a map, a RawValue is
+		// expecting a *new* RawValue, with the TYpe property derived
+		// from the cty information given to us.
+		switch dst.(type) {
+		case *map[string]interface{}:
+			*(dst.(*map[string]interface{})) = r.Value.(map[string]interface{})
+		case *RawValue:
+			val := r.Value.(map[string]interface{})
+			var typ interface{}
+			err := json.Unmarshal([]byte(val["type"].(string)), &typ)
+			if err != nil {
+				// TODO: return error
+			}
+			rv, err := rawValueFromComplexType(typ, val["value"])
+			if err != nil {
+				return err
+			}
+			*(dst.(*RawValue)) = rv
+		default:
+			// TODO: return error
+		}
 	case Object:
-		// TODO: handle ambiguity of json types
+		// this can only be something the user decided explicitly was
+		// an Object; we can't get this from default behavior, so
+		// there's no ambiguity here.
+		switch dst.(type) {
+		case *map[string]interface{}:
+			*(dst.(*map[string]interface{})) = r.Value.(map[string]interface{})
+		default:
+			// TODO: return error
+		}
+	default:
+		return ErrUnhandledType(r.Type)
 	}
-	return ErrUnhandledType(r.Type)
+	return nil
+}
+
+func rawValueFromComplexType(typ, val interface{}) (RawValue, error) {
+	switch v := typ.(type) {
+	// primitive types are represented just as strings,
+	// with the type name in the string itself
+	case string:
+		switch v {
+		case "string":
+			return RawValue{
+				Type:  String,
+				Value: val,
+			}, nil
+		case "number":
+			return RawValue{
+				Type:  Number,
+				Value: 0, // TODO: how to represent numbers?
+			}, nil
+		case "bool":
+			return RawValue{
+				Type:  Bool,
+				Value: val,
+			}, nil
+		default:
+			// TODO: return an error
+		}
+	case []interface{}:
+		// sets, lists, tuples, maps, and objects are
+		// represented as slices, recursive iterations of this
+		// type/value syntax
+		if len(v) < 1 {
+			// TODO: return an error
+		}
+		switch v[0] {
+		case "set":
+			if len(v) < 2 {
+				// TODO: return an error
+			}
+			var vals []RawValue
+			for _, value := range val.([]interface{}) {
+				rv, err := rawValueFromComplexType(v[1], value)
+				if err != nil {
+					// TODO: return an error
+				}
+				vals = append(vals, rv)
+			}
+			return RawValue{
+				Type:  Set,
+				Value: vals,
+			}, nil
+		case "list":
+			if len(v) < 2 {
+				// TODO: return an error
+			}
+			var vals []RawValue
+			for _, value := range val.([]interface{}) {
+				rv, err := rawValueFromComplexType(v[1], value)
+				if err != nil {
+					// TODO: return an error
+				}
+				vals = append(vals, rv)
+			}
+			return RawValue{
+				Type:  List,
+				Value: vals,
+			}, nil
+		case "tuple":
+			if len(v) < len(val.([]interface{}))+1 {
+				// TODO: return an error
+			}
+			var vals []RawValue
+			for pos, value := range val.([]interface{}) {
+				rv, err := rawValueFromComplexType(v[pos+1], value)
+				if err != nil {
+					// TODO: return an error
+				}
+				vals = append(vals, rv)
+			}
+			return RawValue{
+				Type:  Tuple,
+				Value: vals,
+			}, nil
+		case "map":
+			if len(v) < 2 {
+				// TODO: return an error
+			}
+			vals := map[string]RawValue{}
+			for key, value := range val.(map[string]interface{}) {
+				rv, err := rawValueFromComplexType(v[1], value)
+				if err != nil {
+					// TODO: return an error
+				}
+				vals[key] = rv
+			}
+			return RawValue{
+				Type:  Map,
+				Value: vals,
+			}, nil
+		case "object":
+			if len(v) < 2 {
+				// TODO: return an error
+			}
+			vals := map[string]RawValue{}
+			valTypes := v[1].(map[string]interface{})
+			if len(valTypes) != len(val.(map[string]interface{})) {
+				// TODO: return an error
+			}
+			for key, value := range val.(map[string]interface{}) {
+				typ, ok := valTypes[key]
+				if !ok {
+					// TODO: return error
+				}
+				rv, err := rawValueFromComplexType(typ, value)
+				if err != nil {
+					// TODO: return an error
+				}
+				vals[key] = rv
+			}
+			return RawValue{
+				Type:  Object,
+				Value: vals,
+			}, nil
+		default:
+		}
+	}
+	// TODO: return error?
+	return RawValue{}, nil
 }
 
 // Type is used to identify Terraform types.
