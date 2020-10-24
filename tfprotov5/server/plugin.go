e2cbca2609cfd916e81c36b1dd90777bb8e7abcf
Paddy Carver <paddy@hashicorp.com>
2020-10-23 22:41:05 -0700

    Fix error message, make golint happy.

--- tfprotov5/server/plugin.go
+++ tfprotov5/server/plugin.go
@@ -16,15 +16,15 @@ type GRPCProviderPlugin struct {
 }
 
 func (p *GRPCProviderPlugin) Server(*plugin.MuxBroker) (interface{}, error) {
-	return nil, errors.New("terraform-plugin-go only implements gRPC servers.")
+	return nil, errors.New("terraform-plugin-go only implements gRPC servers")
 }
 
 func (p *GRPCProviderPlugin) Client(*plugin.MuxBroker, *rpc.Client) (interface{}, error) {
-	return nil, errors.New("terraform-plugin-go only implements gRPC servers.")
+	return nil, errors.New("terraform-plugin-go only implements gRPC servers")
 }
 
 func (p *GRPCProviderPlugin) GRPCClient(context.Context, *plugin.GRPCBroker, *grpc.ClientConn) (interface{}, error) {
-	return nil, errors.New("terraform-plugin-go only implements gRPC servers.")
+	return nil, errors.New("terraform-plugin-go only implements gRPC servers")
 }
 
 func (p *GRPCProviderPlugin) GRPCServer(broker *plugin.GRPCBroker, s *grpc.Server) error {
