f5827a19da8ce9577f88b73e8a3f56e17d9fd059
Paddy Carver <paddy@hashicorp.com>
2021-06-21 10:49:29 -0700

    Better formatting for root AttributePaths.
    For empty AttributePaths (paths pointing to the root of the
    type/whatever) don't prefix an empty colon, prefix it with `<root>:`.

--- tftypes/attribute_path_error.go
+++ tftypes/attribute_path_error.go
@@ -12,7 +12,13 @@ type AttributePathError struct {
 }
 
 func (a AttributePathError) Error() string {
-	return fmt.Sprintf("%s: %s", a.Path, a.err)
+	var path string
+	if len(a.Path.Steps()) > 0 {
+		path = a.Path.String()
+	} else {
+		path = "<root>"
+	}
+	return fmt.Sprintf("%s: %s", path, a.err)
 }
 
 func (a AttributePathError) Unwrap() error {
