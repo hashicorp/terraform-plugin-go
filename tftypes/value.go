e908832812e301451b0673bf3ecf828b04d727a5
Brian Flad <bflad417@gmail.com>
2022-08-10 12:01:32 -0400

    all: Run Go 1.19 fmt
    To prevent issues with the latest `golangci-lint` in CI.

--- tftypes/value.go
+++ tftypes/value.go
@@ -262,9 +262,9 @@ func (val Value) Copy() Value {
 //
 // * String: string, *string
 //
-// * Number: *big.Float, int64, *int64, int32, *int32, int16, *int16, int8,
-//           *int8, int, *int, uint64, *uint64, uint32, *uint32, uint16,
-//           *uint16, uint8, *uint8, uint, *uint, float64, *float64
+//   - Number: *big.Float, int64, *int64, int32, *int32, int16, *int16, int8,
+//     *int8, int, *int, uint64, *uint64, uint32, *uint32, uint16,
+//     *uint16, uint8, *uint8, uint, *uint, float64, *float64
 //
 // * Bool: bool, *bool
 //
