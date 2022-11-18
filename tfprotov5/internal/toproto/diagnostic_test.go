11ceb487560d03dbd64145c05a37b35f36aceafa
Martin Atkins <mart@degeneration.co.uk>
2022-11-17 18:17:11 -0800

    Tweak diagnostics with invalid UTF-8 so they can pass over the wire
    A correct provider should only ever return valid UTF-8 strings as the
    diagnostic Summary or Detail, but since diagnostics tend to be describing
    unexpected situations and are often derived from errors in downstream
    libraries it's possible that a provider might incorrectly return incorrect
    garbage as part of a diagnostic message.
    
    The protobuf serializer rejects non-UTF8 strings with a generic message
    that is unhelpful to end-users:
        string field contains invalid UTF-8
    
    Here we make the compromise that it's better to make a best effort to
    return a diagnostic that is probably only partially invalid so that the
    end user has a chance of still getting some clue about what problem
    occurred. The new helper functions here achieve that by replacing any
    invalid bytes with a correctly-encoded version of the Unicode Replacement
    Character, which will then allow the string to pass over the wire protocol
    successfully and hopefully end up as an obviously-invalid character in
    the CLI output or web UI that's rendering the diagnostics.
    
    This does introduce some slight additional overhead when returning
    responses, but it should be immaterial for any response that doesn't
    include any diagnostics, relatively minor for responses that include
    valid diagnostics, and only markedly expensive for a diagnostic string
    with invalid bytes that will therefore need to be re-encoded on a
    rune-by-rune basis.

---
+++ tfprotov5/internal/toproto/diagnostic_test.go
@@ -0,0 +1,58 @@
+package toproto
+
+import (
+	"testing"
+)
+
+func TestForceValidUTF8(t *testing.T) {
+	tests := []struct {
+		Input string
+		Want  string
+	}{
+		{
+			"hello",
+			"hello",
+		},
+		{
+			"こんにちは",
+			"こんにちは",
+		},
+		{
+			"baﬄe", // NOTE: "ﬄ" is a single-character ligature
+			"baﬄe", // ligature is preserved exactly
+		},
+		{
+			"wé́́é́́é́́!", // NOTE: These "e" have multiple combining diacritics
+			"wé́́é́́é́́!", // diacritics are preserved exactly
+		},
+		{
+			"😸😾", // Astral-plane characters
+			"😸😾", // preserved exactly
+		},
+		{
+			"\xff\xff",     // neither byte is valid UTF-8
+			"\ufffd\ufffd", // both are replaced by replacement character
+		},
+		{
+			"\xff\xff\xff\xff\xff",           // more than three invalid bytes
+			"\ufffd\ufffd\ufffd\ufffd\ufffd", // still expanded even though it exceeds our initial slice capacity in the implementation
+		},
+		{
+			"t\xffe\xffst",     // invalid bytes interleaved with other content
+			"t\ufffde\ufffdst", // the valid content is preserved
+		},
+		{
+			"\xffこんにちは\xffこんにちは",     // invalid bytes interacting with multibyte sequences
+			"\ufffdこんにちは\ufffdこんにちは", // the valid content is preserved
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.Input, func(t *testing.T) {
+			got := forceValidUTF8(test.Input)
+			if got != test.Want {
+				t.Errorf("wrong result\ngot:  %q\nwant: %q", got, test.Want)
+			}
+		})
+	}
+}
