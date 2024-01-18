a4bc8ea3ba4a4f4a43c9bb5676c4e7a573590d72
Brian Flad <bflad417@gmail.com>
2024-01-18 11:34:15 -0500

    tfprotov5+tfprotov6: Add missing SPDX-License-Identifier headers

--- tfprotov5/internal/toproto/doc.go
+++ tfprotov5/internal/toproto/doc.go
@@ -1,4 +1,5 @@
 // Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
 
 // Package toproto converts terraform-plugin-go tfprotov5 types to Protocol
 // Buffers generated tfplugin5 types.
