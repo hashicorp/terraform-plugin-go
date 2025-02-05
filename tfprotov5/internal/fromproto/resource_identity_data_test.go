7ecc85cdc4e8bfbcaecd771687db8bce362d844b
Ansgar Mertens <ansgar@hashicorp.com>
2025-02-05 18:13:39 +0100

    first bunch of tests for files that already existed (and some for new files that are helpers for the other tests in prior art only)

---
+++ tfprotov5/internal/fromproto/resource_identity_data_test.go
@@ -0,0 +1,20 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package fromproto_test
+
+import (
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5/internal/tfplugin5"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5/internal/toproto"
+)
+
+func testTfplugin5ResourceIdentityData() *tfplugin5.ResourceIdentityData {
+	return toproto.ResourceIdentityData(testTfprotov5ResourceIdentityData())
+}
+
+func testTfprotov5ResourceIdentityData() *tfprotov5.ResourceIdentityData {
+	return &tfprotov5.ResourceIdentityData{
+		IdentityData: testTfprotov5DynamicValue(),
+	}
+}
