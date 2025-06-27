9d8432921998abace11ab6084f282cd3b1476d72
Radek Simko <radek.simko@gmail.com>
2025-06-27 13:57:16 +0100

    Implement ValidateStateStoreConfig + ConfigureStateStore methods

---
+++ tfprotov6/internal/fromproto/state_store.go
@@ -0,0 +1,52 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package fromproto
+
+import (
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6"
+	"github.com/hashicorp/terraform-plugin-go/tfprotov6/internal/tfplugin6"
+)
+
+func ValidateStateStoreRequest(in *tfplugin6.ValidateStateStore_Request) *tfprotov6.ValidateStateStoreRequest {
+	if in == nil {
+		return nil
+	}
+
+	return &tfprotov6.ValidateStateStoreRequest{
+		TypeName: in.TypeName,
+		Config:   DynamicValue(in.Config),
+	}
+}
+
+func ConfigureStateStoreRequest(in *tfplugin6.ConfigureStateStore_Request) *tfprotov6.ConfigureStateStoreRequest {
+	if in == nil {
+		return nil
+	}
+
+	return &tfprotov6.ConfigureStateStoreRequest{
+		TypeName: in.TypeName,
+		Config:   DynamicValue(in.Config),
+	}
+}
+
+func GetStatesRequest(in *tfplugin6.GetStates_Request) *tfprotov6.GetStatesRequest {
+	if in == nil {
+		return nil
+	}
+
+	return &tfprotov6.GetStatesRequest{
+		TypeName: in.TypeName,
+	}
+}
+
+func DeleteStateRequest(in *tfplugin6.DeleteState_Request) *tfprotov6.DeleteStateRequest {
+	if in == nil {
+		return nil
+	}
+
+	return &tfprotov6.DeleteStateRequest{
+		TypeName: in.TypeName,
+		StateId:  in.StateId,
+	}
+}
