79501af6010d051788a99e9665518d0378832911
Austin Valle <austinvalle@gmail.com>
2025-07-01 16:44:23 -0400

    exported types first for protov5

---
+++ tfprotov5/action.go
@@ -0,0 +1,127 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
+package tfprotov5
+
+import (
+	"context"
+	"iter"
+)
+
+// ActionMetadata describes metadata for an action in the GetMetadata
+// RPC.
+type ActionMetadata struct {
+	// TypeName is the name of the action.
+	TypeName string
+}
+
+// ActionServer is an interface containing the methods an action
+// implementation needs to fill.
+type ActionServer interface {
+	// TODO: pkg docs
+	PlanAction(context.Context, *PlanActionRequest) (*PlanActionResponse, error)
+	// TODO: pkg docs
+	InvokeAction(context.Context, *InvokeActionRequest) (*InvokeActionServerStream, error)
+}
+
+// TODO: pkg docs
+type PlanActionRequest struct {
+	// ActionType is the name of the action being called.
+	ActionType string
+
+	LinkedResources []*ProposedLinkedResource
+
+	// TODO: this is just config of the action?
+	ProposedActionData *DynamicValue
+
+	// ClientCapabilities defines optionally supported protocol features for the
+	// PlanAction RPC, such as forward-compatible Terraform behavior changes.
+	ClientCapabilities *PlanActionClientCapabilities
+}
+
+// TODO: pkg docs
+type ProposedLinkedResource struct {
+	PriorState    *DynamicValue
+	PlannedState  *DynamicValue
+	Config        *DynamicValue
+	PriorIdentity *ResourceIdentityData
+}
+
+// TODO: pkg docs
+type PlanActionResponse struct {
+	LinkedResources []*PlannedLinkedResource
+
+	Diagnostics []*Diagnostic
+
+	// Deferred is used to indicate to Terraform that the PlanAction operation
+	// needs to be deferred for a reason.
+	Deferred *Deferred
+}
+
+// TODO: pkg docs
+type PlannedLinkedResource struct {
+	PlannedState    *DynamicValue
+	PlannedIdentity *ResourceIdentityData
+}
+
+// TODO: pkg docs
+type InvokeActionRequest struct {
+	// ActionType is the name of the action being called.
+	ActionType string
+
+	LinkedResources []*InvokeLinkedResource
+
+	// TODO: this is just config of the action? since there is no output from the action itself
+	PlannedActionData *DynamicValue
+}
+
+// TODO: pkg docs
+type InvokeLinkedResource struct {
+	PriorState      *DynamicValue
+	PlannedState    *DynamicValue
+	Config          *DynamicValue
+	PlannedIdentity *ResourceIdentityData
+}
+
+// TODO: pkg docs
+type InvokeActionServerStream struct {
+	Events iter.Seq[InvokeActionEvent]
+}
+
+// TODO: pkg docs
+type InvokeActionEvent struct {
+	Type InvokeActionEventType
+}
+
+// TODO: pkg docs
+type InvokeActionEventType interface {
+	isInvokeActionEventType() // this interface is only implementable in this package
+}
+
+var (
+	_ InvokeActionEventType = ProgressInvokeActionEventType{}
+	_ InvokeActionEventType = CompletedInvokeActionEventType{}
+)
+
+// TODO: pkg docs
+type ProgressInvokeActionEventType struct {
+	Message string
+}
+
+func (a ProgressInvokeActionEventType) isInvokeActionEventType() {}
+
+// TODO: pkg docs
+type CompletedInvokeActionEventType struct {
+	LinkedResources []*NewLinkedResource
+	Diagnostics     []*Diagnostic
+}
+
+func (a CompletedInvokeActionEventType) isInvokeActionEventType() {}
+
+// TODO: pkg docs
+type NewLinkedResource struct {
+	NewState    *DynamicValue
+	NewIdentity *ResourceIdentityData
+
+	RequiresReplace bool
+}
