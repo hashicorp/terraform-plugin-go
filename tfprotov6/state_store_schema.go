24b2c9203bee245fb1c62fd0c3cc4e6ec7c4378d
Rain <rainskwan@gmail.com>
2025-11-19 18:19:36 -0500

    Moved to v6

--- tfprotov6/state_store_schema.go
+++ tfprotov6/state_store_schema.go
@@ -1,10 +1,12 @@
 // Copyright (c) HashiCorp, Inc.
 // SPDX-License-Identifier: MPL-2.0
 
-package tfprotov5
+package tfprotov6
+
+import "github.com/hashicorp/terraform-plugin-go/tfprotov5"
 
 // StateStoreSchema is how Terraform defines the shape of state store data.
 type StateStoreSchema struct {
 	// Schema is the definition for the state store data itself, which will be specified in an state store block in the user's configuration.
-	Schema *Schema
+	Schema *tfprotov5.Schema
 }
