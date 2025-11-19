bf48b8010fad35d3f691923f25609b43b715c72f
Steph <steph@hashicorp.com>
2025-11-19 09:16:10 +0100

    add copyright headers

--- tfprotov5/generate_resource_config.go
+++ tfprotov5/generate_resource_config.go
@@ -1,3 +1,6 @@
+// Copyright (c) HashiCorp, Inc.
+// SPDX-License-Identifier: MPL-2.0
+
 package tfprotov5
 
 // GenerateResourceConfigRequest is the request Terraform sends when it wants to generate configuration
