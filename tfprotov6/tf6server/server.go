a65e8e7e8f7a7bea144a40dbadaad962b8af8d82
Austin Valle <austinvalle@gmail.com>
2026-01-16 08:46:56 -0500

    update protocol version

--- tfprotov6/tf6server/server.go
+++ tfprotov6/tf6server/server.go
@@ -53,7 +53,7 @@ const (
 	//
 	// In the future, it may be possible to include this information directly
 	// in the protocol buffers rather than recreating a constant here.
-	protocolVersionMinor uint = 10
+	protocolVersionMinor uint = 11
 )
 
 // protocolVersion represents the combined major and minor version numbers of
